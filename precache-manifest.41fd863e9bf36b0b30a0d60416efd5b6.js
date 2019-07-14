e19de3561adc8cd4f8d3e3c432c46281f5689672
Ivan Poberezhnyuk <vanyapoberejnuk@gmail.com>
2019-07-15 01:47:20 +0300

    Updates

---
+++ precache-manifest.41fd863e9bf36b0b30a0d60416efd5b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5f380aaa2fa9506911156b3b7483f2",
+    "url": "/kharkiv_test/index.html"
+  },
+  {
+    "revision": "987e7aaadf1c4610a500",
+    "url": "/kharkiv_test/static/css/main.ea355e38.chunk.css"
+  },
+  {
+    "revision": "d5da5fe80422eddcbaee",
+    "url": "/kharkiv_test/static/js/2.f718ff77.chunk.js"
+  },
+  {
+    "revision": "987e7aaadf1c4610a500",
+    "url": "/kharkiv_test/static/js/main.b77fd3a1.chunk.js"
+  },
+  {
+    "revision": "8c0a451ecfad4764b65c",
+    "url": "/kharkiv_test/static/js/runtime~main.eb4f7a29.js"
+  },
+  {
+    "revision": "37ed1e9af42eecd12beeb8b9eabe9765",
+    "url": "/kharkiv_test/static/media/jared-sluyter-342881.37ed1e9a.png"
+  },
+  {
+    "revision": "4a542af00275f6d7f46a3b0103fdffad",
+    "url": "/kharkiv_test/static/media/join-our-team.4a542af0.png"
+  },
+  {
+    "revision": "7f5f65edf2ba5b654c9cd8002cce4bf8",
+    "url": "/kharkiv_test/static/media/mother-and-child.7f5f65ed.png"
+  },
+  {
+    "revision": "537349068cf671f5da38efef5b64228b",
+    "url": "/kharkiv_test/static/media/people_background.53734906.png"
+  }
+]);
